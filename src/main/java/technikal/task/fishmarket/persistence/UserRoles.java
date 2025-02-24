f3f5f5a6cdc74342f581da930b105cf6c528b32c
AlexVar <alexvarushiK@gmail.com>
2025-02-24 19:43:04 +0200

    Remove redundant user components

--- src/main/java/technikal/task/fishmarket/persistence/UserRoles.java
+++
@@ -1,6 +0,0 @@
-package technikal.task.fishmarket.persistence;
-
-public enum UserRoles {
-	USER,
-	OWNER;
-}
--- src/main/java/technikal/task/fishmarket/services/user/UserService.java
+++
@@ -1,8 +0,0 @@
-package technikal.task.fishmarket.services.user;
-
-import technikal.task.fishmarket.pojo.UserReadPojo;
-import technikal.task.fishmarket.pojo.UserWritePojo;
-
-public interface UserService {
-	UserReadPojo create(final UserWritePojo userWritePojo);
-}
